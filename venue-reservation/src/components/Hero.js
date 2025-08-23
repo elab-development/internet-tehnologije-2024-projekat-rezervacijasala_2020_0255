5444304d5205678e14ade53440ab4b545ec9add5
nikolanovkovic <novkovicnikola289@gmail.com>
2025-08-24 00:40:04 +0200

    komponenta dobrodoslice

---
+++ venue-reservation/src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <section className="home">
+      <div className="content">
+        <span>Dobro Došli</span>
+        <h3>
+        Mi činimo <br /> vaše događaje izuzetnim <br />  i neverovatnim.
+        </h3>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
