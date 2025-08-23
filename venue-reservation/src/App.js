548793dd6b85ced93db39ee4f8ca8048396f73b1
nikolanovkovic <novkovicnikola289@gmail.com>
2025-08-24 00:27:26 +0200

    rute

--- venue-reservation/src/App.js
+++ venue-reservation/src/App.js
@@ -1,8 +1,36 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Venues from './pages/Venues';
+import Reservations from './pages/Reservations';
+
 function App() {
+  const user = Boolean(useSelector((state) => state.user));
+
   return (
-    <div >
-      Venue Reservation
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/venues"
+          element={user ? <Venues /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/reservations"
+          element={user ? <Reservations /> : <Navigate to="/" />}
+        />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
